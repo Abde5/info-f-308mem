98b389c98279fd318626ffe074dee26b3f61ba41
Abde El-Haman <aelhaman@ulb.ac.be>
2017-04-16 04:42:56 +0200

    [ADD] fix TSM

--- implementation/timestretch.clj
+++ implementation/timestretch.clj
@@ -1,8 +1,8 @@
 (use 'overtone.core)
 (connect-external-server 57110)
 
-;; Time Stretching following the Phase Vocoder Algorithm
-;; -> I don't think so
+;; synth that plays a buffer, I took it from the overtone source code
+
 (defsynth stereo-partial-player
   "Plays a stereo buffer from start pos to end pos (represented as
    values between 0 and 1). May be looped via the loop?
@@ -21,18 +21,16 @@
     (out out-bus (* amp env snd))))
 
 
-;; Granular live time stretching algorithm
-;; USED TO APPLY SOLA -> play a grain in the
-
+;; Granular live time stretching algorithm (AKA SOLA)
 (defsynth granulator
-  "comment à écrire"
+  "commentaire à écrire"
   [buf 0 rate 1 gr-dur 0.1 start 0 end 1 out-bus 0 loop? 0 amp 1]
   (let [n-frames     (buf-frames buf)
         buf-rate     (buf-rate-scale buf)
         rate         (* rate buf-rate)
         start-pos    (* start n-frames)
         end-pos      (* end n-frames)
-        trigger      (trig1:kr (sin-osc:kr ( / 1 (* gr-dur 0.6))) (* dur 0.4))
+        trigger      (trig1:kr (sin-osc:kr ( / 1 (* gr-dur 0.7))) (* gr-dur 0.4))
         gr-env       (env-gen:kr (envelope [0 1 0] [(/ gr-dur 2) (/ gr-dur 2)] :sine)
                                 trigger :action FREE)
         phase        (phasor:ar :start start-pos :end end-pos :rate rate)
@@ -41,20 +39,12 @@
     (out out-bus (pan2 signal))
    ))
 
-(def playing-granulator (granulator digitalbuf :rate 1 :start 0.1))
-(ctl playing-granulator :rate 1.2)
-
-(stop)
-
 ;;;;;;;;;;;;;;;;;;;;;;;; TESTS ;;;;;;;;;;;;;;;;;;;;;;;;
-(demo (sin-osc))
 
-(def digitalplaying ((sample "digital.wav")))
-(ctl digitalplaying :rate 1.5)
+(def playing-granulator (granulator digitalbuf :rate 1.5 :start 0.1))
+(ctl playing-granulator :rate 1.5)
+
 (stop)
-
-(defsynth playbacker [buf 1]
-  (out 0 (play-buf 1 buf)))
 
 
 (def digitalbuf (load-sample "digital.wav"))
